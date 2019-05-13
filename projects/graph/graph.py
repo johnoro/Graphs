bf77067b68040c5dff725f5f9f4845e0b93f8a9a
johnoro <officialjohnorourke@gmail.com>
2019-05-13 13:55:25 -0400

    Adds testing to ensure r. dft works w/ default args

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -150,6 +150,10 @@ if __name__ == '__main__':
         1, 2, 4, 6, 3, 5, 7
     '''
     graph.dft_recursive(1)
+    # graph.add_vertex(8)
+    # graph.add_edge(2, 8)
+    # graph.add_edge(7, 8)
+    # graph.dft_recursive(1)
 
     '''
     Valid BFS path:
